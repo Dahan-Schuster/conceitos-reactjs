4dd00c14dbd666878f08c4806145f3d99a2f0e72
Dahan Schuster <dan.plschuster@gmail.com>
2020-05-08 14:07:58 -0300

    Cria a página de CRUD de repositórios

--- src/App.jsx
+++ src/App.jsx
@@ -1,39 +1,101 @@
-import React, {useState, useEffect} from "react";
+import React, { useState, useEffect } from "react";
 import api from './services/api'
 
 import "./styles.css";
 
 function App() {
 
-  const repositories = useState([])
+	const [repositories, setRepositories] = useState([])
 
-  useEffect(() => {
-    
-  }, [])
+	useEffect(() => {
+		api.get('/repositories').then(response => {
+			setRepositories(response.data)
+		})
+	}, [])
 
-  async function handleAddRepository() {
-    // TODO
-  }
+	async function handleAddRepository() {
+		let repositoryTitle = document.getElementById('input-repository-title')
+			.value || `New repository ${Date.now()}`
+		let repositoryUrl =
+			document.getElementById('input-repository-url').value || 'https://github.com'
+		let repositoryTech1 =
+			document.getElementById('input-repository-tech-1').value || 'ReactJS'
+		let repositoryTech2 =
+			document.getElementById('input-repository-tech-2').value || ''
 
-  async function handleRemoveRepository(id) {
-    // TODO
-  }
+		let repository = {
+			title: repositoryTitle,
+			url: repositoryUrl,
+			techs: [
+				repositoryTech1, repositoryTech2
+			]
+		}
 
-  return (
-    <div>
-      <ul data-testid="repository-list">
-        <li>
-          Repositório 1
+		api.post('/repositories', repository)
+			.then(reponse =>
+				setRepositories([...repositories, reponse.data])
+			)
+			.catch(error => alert(error))
+	}
 
-          <button onClick={() => handleRemoveRepository(1)}>
-            Remover
-          </button>
-        </li>
-      </ul>
+	async function handleRemoveRepository(id) {
+		api.delete(`/repositories/${id}`)
+			.then(reponse => {
+				let repoIndex = repositories.findIndex(repo => repo.id === id)
+				repositories.splice(repoIndex, 1)
 
-      <button onClick={handleAddRepository}>Adicionar</button>
-    </div>
-  );
+				setRepositories([...repositories])
+			})
+			.catch(error => alert(error))
+	}
+
+	return (
+		<div className="d-flex flex-column container">
+			<h1>Repositories</h1>
+			<div className="d-flex">
+				<div className="list">
+					<h2>Repository list</h2>
+					<ul id="repository-list" data-testid="repository-list">
+						{
+							repositories.map(repository =>
+								<li id={repository.id} key={repository.id}>
+									<p className="d-flex flex-column">
+										<a rel="noopener noreferrer" target="_blank" href={repository.url}>
+											<span>
+												{repository.title}
+											</span>
+										</a>
+										<small>Made with{
+											repository.techs.map(tech => {
+												if (tech.length > 0) {
+													return ` ${tech};`
+												}
+												return ''
+											})
+										}</small>
+									</p>
+									<button onClick={() => handleRemoveRepository(repository.id)}>
+										Remover
+											</button>
+								</li>
+							)
+						}
+					</ul>
+					{
+						repositories.length === 0 && <h3>The repository list is empty</h3>
+					}
+				</div>
+				<div className="form">
+					<h2>Create new repository</h2>
+					<input placeholder="Title" type="text" id="input-repository-title" />
+					<input placeholder="Link to Github" type="text" id="input-repository-url" />
+					<input placeholder="Tech 1" type="text" id="input-repository-tech-1" />
+					<input placeholder="Tech 2" type="text" id="input-repository-tech-2" />
+					<button onClick={handleAddRepository}>Adicionar</button>
+				</div>
+			</div>
+		</div>
+	);
 }
 
 export default App;
